66507e40a9421a96df3d99dbb5c0e0849b868f3e
Joseph Wang <josephwang@letu.edu>
2025-03-12 07:30:24 -0700

    zombie kill rate analytics

---
+++ Assets/Scripts/AnalyticsCommon.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class AnalyticsCommon : MonoBehaviour
+{
+    public static long timeZombieKilled { get; set; }
+
+    //public static long TimeZombieKiled
+    //{
+    //    get { return timeZombieKilled; }
+    //    set { timeZombieKilled = value; }
+    //}
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/SendToGoogle.cs
+++ Assets/Scripts/SendToGoogle.cs
@@ -5,12 +5,14 @@ using UnityEngine.Networking;
 
 public class SendToGoogle : MonoBehaviour
 {
+    public static SendToGoogle Instance { get; private set; } // Singleton instance
+
     [SerializeField] private string URL;
 
     private long _sessionID;
-    private int _testInt;
-    private bool _testBool;
-    private float _testFloat;
+    //private int _testInt;
+    //private bool _testBool;
+    //private float _testFloat;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,27 +20,45 @@ public class SendToGoogle : MonoBehaviour
         
     }
 
-    private void Awake() {
-        _sessionID = DateTime.Now.Ticks;
+    //private void Awake() {
+    //    _sessionID = DateTime.Now.Ticks;
 
-        Send();
+    //    //Send();
+    //}
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Persist across scenes
+            _sessionID = DateTime.Now.Ticks; // Set session ID once
+        }
+        else
+        {
+            Destroy(gameObject); // Prevent duplicate instances
+        }
     }
 
-    public void Send() {
-        _testInt = UnityEngine.Random.Range(0, 101);
-        _testBool = true;
-        _testFloat = UnityEngine.Random.Range(0.0f, 10.0f);
+    public void Send(long timeZombieKilled) {
+        //_testInt = UnityEngine.Random.Range(0, 101);
 
-        StartCoroutine(Post(_sessionID.ToString(), _testInt.ToString(), _testBool.ToString(), _testFloat.ToString()));
+        //_testBool = true;
+        //_testFloat = UnityEngine.Random.Range(0.0f, 10.0f);
+
+        //StartCoroutine(Post(_sessionID.ToString(), _testInt.ToString(), _testBool.ToString(), _testFloat.ToString()));
+        //StartCoroutine(Post(_sessionID.ToString(), AnalyticsCommon.timeZombieKilled.ToString()));
+        StartCoroutine(Post(_sessionID.ToString(), timeZombieKilled.ToString()));
     }
 
-    private IEnumerator Post(string sessionID, string testInt, string testBool, string testFloat)
+    //private IEnumerator Post(string sessionID, string testInt, string testBool, string testFloat)
+    private IEnumerator Post(string sessionID, string timeZombieKilled)
     {
         WWWForm form = new WWWForm();
         form.AddField("entry.15244743", sessionID);
-        form.AddField("entry.251709568", testInt);
-        form.AddField("entry.1284425705", testBool);
-        form.AddField("entry.1132036655", testFloat);
+        form.AddField("entry.251709568", timeZombieKilled);
+        //form.AddField("entry.1284425705", testBool);
+        //form.AddField("entry.1132036655", testFloat);
 
         using (UnityWebRequest www = UnityWebRequest.Post(URL, form))
         {
