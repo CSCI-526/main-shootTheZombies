0da852b191a16fe3b0b40b8a85fdd6af556ba271
lcwnvgp <64069007+lcwnvgp@users.noreply.github.com>
2025-03-14 12:07:56 -0700

    Chan (#11)
    * Implement restart and pause functionality for both level-up and game control
    
    * Remove unnecessary files such as solution files and .DS_Store

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -90,6 +90,7 @@ public class Player : MonoBehaviour
     }
 
     void LevelUp(){
+        Time.timeScale = 0;
         playerLevel += 1;
         // //Debug.Log("Level Up! Current Level: " + playerLevel);
         towerPlayerManager.UnlockTowerType(playerLevel);
@@ -108,51 +109,6 @@ public class Player : MonoBehaviour
         }
         buttonSpawner.InitializeButtons(buttonTexts[0], buttonTexts[1], buttonTexts[2]);
     }
-    // void FireBullet(Vector3 target)
-    // {
-    //     Vector3 direction = (target - transform.position).normalized; 
-    //     float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-    //     // rotate
-    //     GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.Euler(0, 0, angle+90));
-
-    //     Bullet bulletScript = bullet.GetComponent<Bullet>();
-    //     bulletScript.Initialize(target);
-    // }
-
-    // Zombie FindZombieAtPosition(Vector3 position)
-    // {
-    //     float detectionRadius = 1.5f;
-    //     Collider2D[] colliders = Physics2D.OverlapCircleAll(position, detectionRadius);
-
-    //     foreach (Collider2D collider in colliders)
-    //     {
-    //         if (collider.CompareTag("Zombie"))
-    //         {
-    //             return collider.GetComponent<Zombie>();
-    //         }
-    //     }
-    //     return null;
-    // }
-
-    // Zombie FindNearestZombie(Vector3 playerPosition)
-    // {
-    //     Zombie[] zombies = FindObjectsOfType<Zombie>();
-    //     Zombie nearestZombie = null;
-    //     float shortestDistance = Mathf.Infinity;
-
-    //     foreach (Zombie zombie in zombies)
-    //     {
-    //         float distance = Vector3.Distance(playerPosition, zombie.transform.position);
-    //         if (distance < shortestDistance)
-    //         {
-    //             shortestDistance = distance;
-    //             nearestZombie = zombie;
-    //         }
-    //     }
-
-    //     return nearestZombie;
-    // }
 
 }
 
